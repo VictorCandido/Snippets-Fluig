dce1423755a151fc8be5d7145a160305b76e42d4
Victor Candido <victor.candido@iv2.com.br>
2019-10-24 15:05:51 -0300

    Melhorando requisições usando async await

--- Postagens/script.js
+++ Postagens/script.js
@@ -32,61 +32,22 @@ console.info("Iniciando requisição, aguarde...");
 getComunidades().then(comunidadesData => {
     const comunidades = comunidadesData.content.filter(e => e.name.startsWith('Comunicação Interna'));
 
-    comunidades.forEach(comunidade => {
+    comunidades.forEach(async comunidade => {
         const communityAlias = comunidade.alias;
 
-        getComunidadePosts(communityAlias).then(postData => {
-            const posts = postData.content;
+        const postData = await getComunidadePosts(communityAlias);
+        const posts = postData.content;
+        
+        posts.forEach(async post => {
+            const attachments = post.attachments;
 
-            posts.forEach(post => {
-                const attachments = post.attachments;
+            attachments.forEach(async attachment => {
+                const documentId = new URL(`http://url.com${attachment.url}`).searchParams.get('app_ecm_navigation_doc');
 
-                attachments.forEach(attachment => {
-                    const documentId = new URL(`http://url.com${attachment.url}`).searchParams.get('app_ecm_navigation_doc');
-                    
-                    getDocument(documentId).then(documentData => {
-                        const document = documentData.content;
-                        console.log(document.fileURL);
-                    })
-                });
+                const documentData = await getDocument(documentId);
+                const document = documentData.content;
+                console.log(document.fileURL);
             });
         });
     })
 });
-
-// FLUIGC.loading(window).show()
-// console.info("Iniciando requisição, aguarde...");
-
-// getComunidades().then(async comunidadesData => {
-//     const comunidades = comunidadesData.content.filter(e => e.name.startsWith('Comunicação Interna'));
-
-//     for(var i in comunidades) {
-//         const communityAlias = comunidades[i].alias;
-
-//         const postData = await getComunidadePosts(communityAlias);
-//         const posts = postData.content;
-
-//         for(var j in posts) {
-//             const attachments = posts[j].attachments;
-
-//             for(var k in attachments){
-//                 const documentId = new URL(`http://url.com${attachments[k].url}`).searchParams.get('app_ecm_navigation_doc');
-
-//                 const documentData = await getDocument(documentId);
-//                 const document = documentData.content;
-//                 console.log(document.fileURL);
-//             }
-
-//             // attachments.forEach(attachment => {
-//             //     const documentId = new URL(`http://url.com${attachment.url}`).searchParams.get('app_ecm_navigation_doc');
-                
-//             //     getDocument(documentId).then(documentData => {
-//             //         const document = documentData.content;
-//             //         console.log(document.fileURL);
-//             //     })
-//             // });
-//         }
-//     }
-
-//     FLUIGC.loading(window).hide()
-// });
